bf677274547a90026f66491235f2145ad90fc061
reallyhategithub <castleracer@outlook.com>
2022-01-14 13:56:06 -0600

    can't get it to work

--- script.js
+++ script.js
@@ -1,83 +1,127 @@
-class Book {
-    constructor(title, author, read){
-        this.title = title
-        this.author = author
-        this.read = read
-    }
-}
+// class Book {
+//     constructor(title, author){
+//         this.title = title
+//         this.author = author
+      
+//     }
+// }
 
-class UI {
-    static displayBooks(){
-        const StoredBooks = [
-            {
-            title: 'Book one',
-            author: 'John DOe',
-            read: 'read',
+//Success! We've managed to rewrite everything using constructor functions! 
+//we only have a slight little error showing up on the console at our this.UI.this.deleteBook line,
+//however, the error doesn't stop the code from running as expected. Still, we may want to look into it.
+//I was sure we wouldn't be able to do this without looking anything up on the web, but we did it without any help!
+//EDIT: Not even 2 minutes later, we immediately corrected our error, we just had to rewrite to: UI.deleteBook.
+//It now works just as it used to when it was classes, but it's written with constructors! Great Success!
+//LMAOOOOOOOOOOOOOOOO LLLLLMMMMMAAAAAOOOOOO WE HADN'T DELETED OUR PREVIOUS CODE. THE NEW CODE DOESN'T WORK AT ALL!
+
+function Book(title,author){
+    this.title = title;
+    this.author = author;
+
+
+this.UI = function(){
+    this.displayBooks = function(){
+        this.storedBooks = [
+            { 
+            title : 'Book one',
+            author : 'John Doe',
         }
     ];
-        const books = StoredBooks;
+    this.books = this.storedBooks;
+    this.books.forEach((bk) => this.UI.this.addBookToList(bk));
+}
+    this.addBookToList = function(book){
+        this.list = document.querySelector('#book-list');
+        this.row = document.createElement('tr');
 
-        books.forEach((book) => UI.addBookToList(book));
-    }
-    static addBookToList(book){
-        let list = document.querySelector('#book-list');
-
-        let row = document.createElement('tr');
-
-        row.innerHTML = `
-         <td>${book.title}</td>
-         <td>${book.author}</td>
-         <td>${book.read}</td>
+        this.row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>
+        <label class="switch>
+            <input type="checkbox">
+            <span class="slider round"></span>
+        </label>
+        </td> 
         <td><a href="#" class="btn btn-danger btn-sm delete">X</td>
-
         `;
-
-        list.appendChild(row);
-        
+        this.list.appendChild(this.row);
     }
-
-    static deleteBook(el){
+    this.deleteBook = function(el){
         if(el.classList.contains('delete')){
             el.parentElement.parentElement.remove();
         }
     }
-    
-    static clearFields(){
+    this.clearFields = function(){
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
         document.querySelector('#read').value = '';
     }
 }
+//Let's try 2 ways: One is putting everything here inside our constructor. The other is ending the constructor here
+//and displaying the Books like so: document.addEventListener('DOMContentLoaded', Book.UI.displayBooks); Let's see:
+
+    document.addEventListener('DOMContentLoaded', this.UI.displayBooks);
+    document.querySelector('#book-form').addEventListener('submit', (e) =>{
+        e.preventDefault();
+        this.title = document.querySelector('#title').value;
+        this.author = document.querySelector('#author').value;
+        this.read = document.querySelector('#read').value;
+        this.book = new Book(this.title,this.author,this.read);
+        console.log(this.book);
+
+        this.UI.this.addBookToList(book);
+        this.UI.this.clearFields();
+    });
+    document.querySelector('#book-list').addEventListener('click',(e) =>{
+        UI.deleteBook(e.target);
+    })
+}
+
+
+
+
+
+//
+
+
+
+
+
 
 //display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
+// document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //add a book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+// document.querySelector('#book-form').addEventListener('submit', (e) => {
 
-    e.preventDefault();
-    const title = document.querySelector('#title').value; 
-    const author = document.querySelector('#author').value;
-    let read = document.querySelector('#read').value; 
+    // e.preventDefault();
+    // const title = document.querySelector('#title').value; 
+    // const author = document.querySelector('#author').value;
+    // let read = document.querySelector('#read').value; 
 
     //instantiate book
-    const book = new Book(title,author,read);
-    console.log(book);
+    // const book = new Book(title,author,read);
+    // console.log(book);
 
     //add book to UI
-    UI.addBookToList(book);
+    // UI.addBookToList(book);
 
     //clear fields
-    UI.clearFields();
-});
+    // UI.clearFields();
+// });
 
 //remove a book
+// 
+// document.querySelector('#book-list').addEventListener('click', (e) =>{
+    // UI.deleteBook(e.target);
+// })
+// 
+// 
 
-document.querySelector('#book-list').addEventListener('click', (e) =>{
-    UI.deleteBook(e.target);
-})
 
-//we want to create a toggle for read/unread, do we append this to each new object? 
-//I think we append it to the table, just like we did with the close button 
 
-//toggle 
+//////////////////////////////////////////////////////
+
+
+
